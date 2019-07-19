f2cddaefc1a26b13b2f9f9a50efc8a3a6b1fe748
Γιάννης Μικές <mikesgiannis@gmail.com>
2019-07-20 00:00:40 +0300

    Accessor functions για Piece

--- Chess/Piece.cpp
+++ Chess/Piece.cpp
@@ -1,1 +1,31 @@
 #include "Piece.h"
+
+void Piece::setIsAlive(bool alive) {
+	this->isAlive = alive;
+}
+
+void Piece::setHasMoved(bool moved) {
+	this->hasMoved = moved;
+}
+
+void Piece::setColor(bool color) {
+	this->color = color;
+}
+
+void Piece::setPosX(int x) {
+	this->posX = x;
+}
+
+void Piece::setPosY(int y) {
+	this->posY = y;
+}
+
+bool Piece::getIsAlive() { return this->isAlive; }
+
+bool Piece::getHasMoved() { return this->hasMoved; }
+
+bool Piece::getColor() { return this->color; }
+
+int Piece::getPosX() { return this->posX; }
+
+int Piece::getPosY() { return this->posY; }
--- Chess/Piece.h
+++ Chess/Piece.h
@@ -12,4 +12,17 @@ protected:
 	int posY;
 public:
 	virtual bool checkMove(int, int) = 0;		//ελεγχος εγκυρης κινησης
+	
+	//accessor functions
+	void setIsAlive(bool);
+	void setHasMoved(bool);
+	void setColor(bool);
+	void setPosX(int);
+	void setPosY(int);
+
+	bool getIsAlive();
+	bool getHasMoved();
+	bool getColor();
+	int getPosX();
+	int getPosY();
 };
